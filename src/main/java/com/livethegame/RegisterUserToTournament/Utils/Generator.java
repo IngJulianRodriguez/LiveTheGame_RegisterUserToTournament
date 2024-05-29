480f5b74a3f29b42947d578ba542b8fcdc0ec87a
Julian Rodriguez <jurodr@bancolombia.com.co>
2024-05-29 16:29:40 -0500

    fix error en generador de codigos

--- src/main/java/com/livethegame/RegisterUserToTournament/Utils/Generator.java
+++ src/main/java/com/livethegame/RegisterUserToTournament/Utils/Generator.java
@@ -8,6 +8,6 @@ import com.livethegame.RegisterUserToTournament.entities.Users;
 public  class Generator {
     public static String generateCode(){
         double code = 100000 + Math.random() * 900000;
-        return String.valueOf(code);
+        return String.valueOf(code).substring(0,6);
     };
 }
